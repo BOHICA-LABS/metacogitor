acf593c4ec0a6e766d243d5403aee193724b2518
Joshua Magady <josh.magady@gmail.com>
2023-08-18 19:28:59 -0500

    feat: add cost_manager

--- src/metacogitor/utils/__init__.py
+++ src/metacogitor/utils/__init__.py
@@ -1,1 +1,3 @@
 from metacogitor.utils.rate_limiter import *
+from metacogitor.utils.cost_manager import *
+from metacogitor.utils.token_counter import *
--- src/metacogitor/utils/cost_manager.py
+++ src/metacogitor/utils/cost_manager.py
@@ -7,6 +7,17 @@
 """
 
 from typing import NamedTuple
+from metacogitor.utils.singleton import Singleton
+from metacogitor.utils.token_counter import (
+    TOKEN_COSTS,
+    count_message_tokens,
+    count_string_tokens,
+    get_max_completion_tokens,
+)
+from metacogitor.logs import logger
+from metacogitor.config import CONFIG
+
+__ALL__ = ["Costs", "CostManager"]
 
 
 class Costs(NamedTuple):
@@ -34,3 +45,87 @@ class Costs(NamedTuple):
 
     total_budget: float
     """The maximum budget allowed."""
+
+
+class CostManager(metaclass=Singleton):
+    """Manages costs incurred from using the AI assistant.
+
+    This singleton class tracks the number of tokens, costs,
+    and budget when making API calls to the AI assistant.
+    """
+
+    def __init__(self):
+        self.total_prompt_tokens = 0
+        """Total number of prompt tokens used."""
+
+        self.total_completion_tokens = 0
+        """Total number of completion tokens generated."""
+
+        self.total_cost = 0
+        """Total cost accrued so far."""
+
+        self.total_budget = 0
+        """Total budget allowed."""
+
+    def update_cost(self, prompt_tokens, completion_tokens, model):
+        """Update the total cost, prompt tokens, and completion tokens.
+
+        Args:
+            prompt_tokens (int): Number of prompt tokens used.
+            completion_tokens (int): Number of completion tokens generated.
+            model (str): The AI model used.
+        """
+
+        self.total_prompt_tokens += prompt_tokens
+        self.total_completion_tokens += completion_tokens
+        cost = (
+            prompt_tokens * TOKEN_COSTS[model]["prompt"]
+            + completion_tokens * TOKEN_COSTS[model]["completion"]
+        ) / 1000
+        self.total_cost += cost
+        logger.info(
+            f"Total running cost: ${self.total_cost:.3f} | Max budget: ${CONFIG.max_budget:.3f} | "
+            f"Current cost: ${cost:.3f}, prompt_tokens: {prompt_tokens}, completion_tokens: {completion_tokens}"
+        )
+        CONFIG.total_cost = self.total_cost
+
+    def get_total_prompt_tokens(self):
+        """Get the total number of prompt tokens used.
+
+        Returns:
+            int: Total prompt tokens.
+        """
+
+        return self.total_prompt_tokens
+
+    def get_total_completion_tokens(self):
+        """Get the total number of completion tokens generated.
+
+        Returns:
+            int: Total completion tokens.
+        """
+
+        return self.total_completion_tokens
+
+    def get_total_cost(self):
+        """Get the total cost accrued so far.
+
+        Returns:
+            float: The total cost.
+        """
+
+        return self.total_cost
+
+    def get_costs(self) -> Costs:
+        """Get Costs tuple with current token, cost and budget info.
+
+        Returns:
+            Costs: Named tuple instance holding cost info.
+        """
+
+        return Costs(
+            self.total_prompt_tokens,
+            self.total_completion_tokens,
+            self.total_cost,
+            self.total_budget,
+        )
