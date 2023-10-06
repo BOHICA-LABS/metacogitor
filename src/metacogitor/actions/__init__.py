519587ce5fabd15cf1f95363303fc7d493a70a3d
Joshua Magady <josh.magady@gmail.com>
2023-10-06 12:38:37 -0500

    feat: Add updated ActionOutput class for returning complex responses

--- src/metacogitor/actions/__init__.py
+++ src/metacogitor/actions/__init__.py
@@ -1,2 +1,66 @@
-#from metacogitor.actions.base_action import *
-from metacogitor.actions.action_output import *
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2023/5/11 17:44
+@Author  : alexanderwu
+@File    : __init__.py
+"""
+from enum import Enum
+
+from metacogitor.actions.action import Action
+from metacogitor.actions.action_output import ActionOutput
+from metacogitor.actions.add_requirement import BossRequirement
+from metacogitor.actions.debug_error import DebugError
+from metacogitor.actions.design_api import WriteDesign
+from metacogitor.actions.design_api_review import DesignReview
+from metacogitor.actions.design_filenames import DesignFilenames
+from metacogitor.actions.project_management import AssignTasks, WriteTasks
+from metacogitor.actions.research import (
+    CollectLinks,
+    WebBrowseAndSummarize,
+    ConductResearch,
+)
+from metacogitor.actions.run_code import RunCode
+from metacogitor.actions.search_and_summarize import SearchAndSummarize
+from metacogitor.actions.write_code import WriteCode
+from metacogitor.actions.write_code_review import WriteCodeReview
+from metacogitor.actions.write_prd import WritePRD
+from metacogitor.actions.write_prd_review import WritePRDReview
+from metacogitor.actions.write_test import WriteTest
+from metacogitor.actions.youtube_download import YouTubeDownloadAction
+from metacogitor.actions.extract_audio import ExtractAudioFromVideoAction
+from metacogitor.actions.local_transcribe import LocalTranscribe
+from metacogitor.actions.write_atomic_notes import WriteAtomicNotes
+
+
+class ActionType(Enum):
+    """All types of Actions, used for indexing."""
+
+    ADD_REQUIREMENT = BossRequirement
+    WRITE_PRD = WritePRD
+    WRITE_PRD_REVIEW = WritePRDReview
+    WRITE_DESIGN = WriteDesign
+    DESIGN_REVIEW = DesignReview
+    DESIGN_FILENAMES = DesignFilenames
+    WRTIE_CODE = WriteCode
+    WRITE_CODE_REVIEW = WriteCodeReview
+    WRITE_TEST = WriteTest
+    RUN_CODE = RunCode
+    DEBUG_ERROR = DebugError
+    WRITE_TASKS = WriteTasks
+    ASSIGN_TASKS = AssignTasks
+    SEARCH_AND_SUMMARIZE = SearchAndSummarize
+    COLLECT_LINKS = CollectLinks
+    WEB_BROWSE_AND_SUMMARIZE = WebBrowseAndSummarize
+    CONDUCT_RESEARCH = ConductResearch
+    YOUTUBE_DOWNLOAD = YouTubeDownloadAction
+    EXTRACT_AUDIO = ExtractAudioFromVideoAction
+    LOCAL_TRANSCRIBE = LocalTranscribe
+    WRITE_ATOMIC_NOTES = WriteAtomicNotes
+
+
+__all__ = [
+    "ActionType",
+    "Action",
+    "ActionOutput",
+]
--- src/metacogitor/actions/action_output.py
+++ src/metacogitor/actions/action_output.py
@@ -1,76 +1,43 @@
 #!/usr/bin/env python
 # coding: utf-8
 """
-@Time    : 2023/8/14 10:03
-@Author  : Joshua Magady
+@Time    : 2023/7/11 10:03
+@Author  : chengmaoyu
 @File    : action_output
-@Description: This defines the action output class for the metacogitor project.
 """
 
 from typing import Dict, Type
 
 from pydantic import BaseModel, create_model, root_validator, validator
 
-__ALL__ = ["ActionOutput"]
-
 
 class ActionOutput:
-    """
-    Represents the output of an action.
-    """
+    content: str
+    instruct_content: BaseModel
 
     def __init__(self, content: str, instruct_content: BaseModel):
-        """
-        Initialize an ActionOutput instance.
-
-        :param content: Content of the output.
-        :param instruct_content: Instruct content as a BaseModel instance.
-        """
         self.content = content
         self.instruct_content = instruct_content
 
     @classmethod
-    def create_model_class(cls, class_name: str, mapping: (Dict[str, Type], ...)):
-        """
-        Create a new model class based on the provided class name and mapping.
-
-        :param class_name: Name of the new model class.
-        :param mapping: Mapping of field names to types.
-        :return: New model class.
-        """
+    def create_model_class(cls, class_name: str, mapping: Dict[str, Type]):
         new_class = create_model(class_name, **mapping)
 
-        # TODO: Update this to use the new pydantic syntax for validators
-        @validator("*", allow_reuse=True)
+        @validator('*', allow_reuse=True)
         def check_name(v, field):
-            """
-            Validator to check the field name.
-
-            :param v: Field value.
-            :param field: Field instance.
-            :return: Validated field value.
-            """
             if field.name not in mapping.keys():
-                raise ValueError(f"Unrecognized block: {field.name}")
+                raise ValueError(f'Unrecognized block: {field.name}')
             return v
 
-        # TODO: Update this to use the new pydantic syntax for validators
         @root_validator(pre=True, allow_reuse=True)
         def check_missing_fields(values):
-            """
-            Root validator to check missing fields.
-
-            :param values: Field values.
-            :return: Validated field values.
-            """
             required_fields = set(mapping.keys())
             missing_fields = required_fields - set(values.keys())
             if missing_fields:
-                raise ValueError(f"Missing fields: {missing_fields}")
+                raise ValueError(f'Missing fields: {missing_fields}')
             return values
 
         new_class.__validator_check_name = classmethod(check_name)
-        new_class.__root_validator_check_missing_fields = classmethod(
-            check_missing_fields
-        )
+        new_class.__root_validator_check_missing_fields = classmethod(check_missing_fields)
         return new_class
+    
