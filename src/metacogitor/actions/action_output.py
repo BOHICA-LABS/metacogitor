620fb0893f9fed15f7b5eede9295af193541dff8
Joshua Magady <josh.magady@gmail.com>
2023-10-06 12:42:24 -0500

    docs: updated doc strings in action outputs

--- src/metacogitor/actions/action_output.py
+++ src/metacogitor/actions/action_output.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-"""
-@Time    : 2023/7/11 10:03
-@Author  : chengmaoyu
-@File    : action_output
-"""
+# -*- coding: utf-8 -*-
 
 from typing import Dict, Type
 
@@ -12,6 +6,14 @@ from pydantic import BaseModel, create_model, root_validator, validator
 
 
 class ActionOutput:
+    """
+    Represents the output of an action.
+
+    Attributes:
+        content (str): The content of the output.
+        instruct_content (BaseModel): The content of the output as a BaseModel.
+    """
+
     content: str
     instruct_content: BaseModel
 
@@ -21,23 +23,47 @@ class ActionOutput:
 
     @classmethod
     def create_model_class(cls, class_name: str, mapping: Dict[str, Type]):
+        """
+        Create a model class for the ActionOutput class.
+
+        Args:
+            :param class_name:
+            :param mapping:
+            :return:
+        """
         new_class = create_model(class_name, **mapping)
 
-        @validator('*', allow_reuse=True)
+        @validator("*", allow_reuse=True)
         def check_name(v, field):
+            """
+            Check the name of the field.
+
+            Args:
+                :param v: The value of the field.
+                :param field: The field.
+                :return: The value of the field.
+            """
             if field.name not in mapping.keys():
-                raise ValueError(f'Unrecognized block: {field.name}')
+                raise ValueError(f"Unrecognized block: {field.name}")
             return v
 
         @root_validator(pre=True, allow_reuse=True)
         def check_missing_fields(values):
+            """
+            Check for missing fields.
+
+            Args:
+                :param values: The values of the fields.
+                :return: The values of the fields.
+            """
             required_fields = set(mapping.keys())
             missing_fields = required_fields - set(values.keys())
             if missing_fields:
-                raise ValueError(f'Missing fields: {missing_fields}')
+                raise ValueError(f"Missing fields: {missing_fields}")
             return values
 
         new_class.__validator_check_name = classmethod(check_name)
-        new_class.__root_validator_check_missing_fields = classmethod(check_missing_fields)
+        new_class.__root_validator_check_missing_fields = classmethod(
+            check_missing_fields
+        )
         return new_class
-    
