de51e6e31014ec539114264ad0584e44124e40a6
Joshua Magady <josh.magady@gmail.com>
2023-10-07 18:54:26 -0500

    feat: add Memory Module

---
+++ src/metacogitor/memory/__init__.py
@@ -0,0 +1,11 @@
+"""Memory Module"""
+# -*- coding: utf-8 -*-
+
+from metacogitor.memory.memory import Memory
+from metacogitor.memory.longterm_memory import LongTermMemory
+
+
+__all__ = [
+    "Memory",
+    "LongTermMemory",
+]
