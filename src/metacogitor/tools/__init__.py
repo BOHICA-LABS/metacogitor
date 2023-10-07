ee9fcdde80e56ad0dc0f7fc0ffd4442b01ffb5ac
Joshua Magady <josh.magady@gmail.com>
2023-10-07 11:40:17 -0500

    feat: Add the tools module

--- src/metacogitor/tools/__init__.py
+++ src/metacogitor/tools/__init__.py
@@ -1,42 +1,22 @@
+"""This module contains the tools used by the metacogitor package."""
 # -*- coding: utf-8 -*-
-"""
-@Time    : 2023/8/18 15:35
-@Author  : Joshua Magady
-@File    : __init__.py
-@Desc    : This defines the tools package.
-"""
-
 
 from enum import Enum
 
 
 class SearchEngineType(Enum):
-    """Enumeration for supported search engines."""
+    """Enum for search engine types."""
 
     SERPAPI_GOOGLE = "serpapi"
-    """Use SerpApi to search Google."""
-
     SERPER_GOOGLE = "serper"
-    """Use Serper to search Google."""
-
     DIRECT_GOOGLE = "google"
-    """Search Google directly."""
-
     DUCK_DUCK_GO = "ddg"
-    """Search DuckDuckGo."""
-
     CUSTOM_ENGINE = "custom"
-    """Use a custom search engine."""
 
 
 class WebBrowserEngineType(Enum):
-    """Enumeration for supported web browser automation engines."""
+    """Enum for web browser engine types."""
 
     PLAYWRIGHT = "playwright"
-    """Use Playwright for browser automation."""
-
     SELENIUM = "selenium"
-    """Use Selenium for browser automation."""
-
     CUSTOM = "custom"
-    """Use a custom browser automation engine."""
