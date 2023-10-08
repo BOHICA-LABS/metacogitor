2002fd7681568f64e22b89d7c560371cdf1a12a2
Joshua Magady <josh.magady@gmail.com>
2023-10-08 12:12:10 -0500

    docs: add search with a specific engine

---
+++ examples/search_with_specific_engine.py
@@ -0,0 +1,22 @@
+"""Search with specific engine"""
+# -*- coding: utf-8 -*-
+
+import asyncio
+
+from metacogitor.roles import Searcher
+from metacogitor.tools import SearchEngineType
+
+
+async def main():
+    # Serper API
+    # await Searcher(engine = SearchEngineType.SERPER_GOOGLE).run(["What are some good sun protection products?","What are some of the best beaches?"])
+    # SerpAPI
+    # await Searcher(engine=SearchEngineType.SERPAPI_GOOGLE).run("What are the best ski brands for skiers?")
+    # Google API
+    await Searcher(engine=SearchEngineType.DIRECT_GOOGLE).run(
+        "What are the most interesting human facts?"
+    )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
