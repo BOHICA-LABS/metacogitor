653f8da5dc5854f81ee56add3023876099aadb3b
Joshua Magady <josh.magady@gmail.com>
2023-10-08 11:48:41 -0500

    feat: Update and add structure action prompt

---
+++ src/metacogitor/prompts/structure_action.py
@@ -0,0 +1,18 @@
+"""Structure of the action prompt."""
+# -*- coding: utf-8 -*-
+
+
+ACTION_SYSTEM = """SYSTEM:
+You serve as an assistant that helps me play Minecraft.
+I will give you a sentence. Please convert this sentence into one or several actions according to the following instructions.
+Each action should be a tuple of four items, written in the form (’verb’, ’object’, ’tools’, ’materials’)
+’verb’ is the verb of this action.
+’object’ refers to the target object of the action.
+’tools’ specifies the tools required for the action.
+’material’ specifies the materials required for the action.
+If some of the items are not required, set them to be ’None’.
+"""
+
+ACTION_USER = """USER:
+The sentence is {sentence}. Generate the action tuple according to the requirements.
+"""
